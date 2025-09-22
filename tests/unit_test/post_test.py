20ec68be85fb5b585a2e74fbed13df3cc11fbf04
Mihail Mihaylov <chakala1975@gmail.com>
2025-09-22 11:34:56 +0300

    Update post tests

--- tests/unit_test/post_test.py
+++ tests/unit_test/post_test.py
@@ -1,18 +1,35 @@
-__author__ = 'Mihail Mihaylov'
-import unittest
+__author__ = "Mihail Mihaylov"
 
+import unittest
 from post import Post
 
 
-class PostTest(unittest.TestCase):
-    def test_create_post(self):
-        post = Post('Test', 'Test Content')
-        self.assertEqual('Test', post.title)
-        self.assertEqual('Test Content', post.content)
+class TestPost(unittest.TestCase):
+    def setUp(self) -> None:
+        self.post = Post("Test", "Test Content")
 
-    def test_json(self):
-        post = Post('Test', 'Test Content')
-        expected = {'title': 'Test',
-                    'content': 'Test Content'}
+    # --- init ---
 
-        self.assertDictEqual(expected, post.json())
+    def test_post_title_is_set(self) -> None:
+        """Asserts that the post title is set on init."""
+        self.assertEqual("Test", self.post.title)
+
+    def test_post_content_is_set(self) -> None:
+        """Asserts that the post content is set on init."""
+        self.assertEqual("Test Content", self.post.content)
+
+    # --- json ---
+
+    def test_json_has_correct_title(self) -> None:
+        """Asserts that json() contains the correct title."""
+        data = self.post.json()
+        self.assertEqual("Test", data["title"])
+
+    def test_json_has_correct_content(self) -> None:
+        """Asserts that json() contains the correct content."""
+        data = self.post.json()
+        self.assertEqual("Test Content", data["content"])
+
+
+if __name__ == "__main__":
+    unittest.main(verbosity=2)
